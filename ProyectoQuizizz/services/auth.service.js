9af3da37861c86d889ebf6f7b32b19b26a1056f4
Anahi <anahi7776@gmail.com>
2025-12-02 19:20:30 -0400

    feat: refactor registration function to use Estudiante and Admin models

--- ProyectoQuizizz/services/auth.service.js
+++ ProyectoQuizizz/services/auth.service.js
@@ -1,8 +1,21 @@
-import { User } from '../models/user.model.js';
+import { Estudiante } from '../models/estudiante.js';
+import { Admin } from '../models/admin.js';
 async function register(nombre, password, rol){
     try{
-        const user = new User({nombre, password, rol});
-        return await user.save();
+        const flag = await Estudiante.findOne({ email: datosEstudiante.email });
+        if (flag) {
+            throw new Error('El correo electronico ya esta registrado');
+        }
+        let respuesta;
+        if(rol === 'admin'){
+            const nuevoAdmin = await Admin.create(datosEstudiante);
+            respuesta = nuevoAdmin.toObject();
+        }else{
+            const nuevoEstudiante = await Estudiante.create(datosEstudiante);
+            respuesta = nuevoEstudiante.toObject();
+        }
+        delete respuesta.password;
+        return respuesta;
     }catch(err){
         throw new Error('Error al registrar: '+ err.message);
     }
