b15683f70d4d73660e2efdb285714cc9e64af803
Ignacio Jaldin <ignaciojaldin72@gmail.com>
2025-12-02 19:40:04 -0400

    feat(login): endpoints

--- ProyectoQuizizz/controllers/auth.controller.js
+++ ProyectoQuizizz/controllers/auth.controller.js
@@ -1,15 +1,34 @@
 import authService from '../services/auth.service.js';
 
-async function register(req,res){
-    try{
-        const {nombre, password, rol} = req.body;
+async function register(req, res) {
+    try {
+        const { nombre, password, rol } = req.body;
         const data = await authService.register(nombre, password, rol);
-        res.status(200).json({success:true, message:"Registro exitoso",data});
-    }catch(err){
-        res.status(500).json({success: false, message: err.message});
+        res.status(200).json({ success: true, message: "Registro exitoso", data });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
     }
+}
+
+
+async function login(req, res) {
+    try {
+        const { nombre, password, email, rol } = req.body;
+        const data = await authService.login(nombre, password, email, rol);
+        res.status(200).json({
+            succes: true,
+            message: "Login exitoso"
+        });
+    } catch (err) {
+        res.status(500).json({
+            sucess: false,
+            message: "error al logear" + err.message
+        });
+    }
+
 }
 
 modules.exports = {
     register,
+    login
 }
--- ProyectoQuizizz/services/auth.service.js
+++ ProyectoQuizizz/services/auth.service.js
@@ -1,7 +1,7 @@
 import { Estudiante } from '../models/estudiante.js';
 import { Admin } from '../models/admin.js';
-async function register(nombre, password, email, rol){
-    try{
+async function register(nombre, password, email, rol) {
+    try {
         const flag = await Estudiante.findOne({ email: email });
         if (flag) {
             throw new Error('El correo electronico ya esta registrado');
@@ -12,20 +12,44 @@ async function register(nombre, password, email, rol){
             nombre,
             password
         }
-        if(rol === 'admin'){
+        if (rol === 'admin') {
             const nuevoAdmin = await Admin.create(datos);
             res = nuevoAdmin.toObject();
-        }else{
+        } else {
             const nuevoEstudiante = await Estudiante.create(datos);
             res = nuevoEstudiante.toObject();
         }
         delete res.password;
         return res;
-    }catch(err){
-        throw new Error('Error al registrar: '+ err.message);
+    } catch (err) {
+        throw new Error('Error al registrar: ' + err.message);
     }
 }
 
+export async function login(nombre, password, email, rol) {
+
+    try {
+        if (rol === 'admin') {
+            const estudiante = await Estudiante.findOne({
+                nombre: nombre,
+                email: email
+            });
+            return estudiante.compararPassword(password);
+        } else {
+            const admin = await Admin.findOne({
+                nombre: nombre,
+                email: email
+            });
+            return admin.compararPassword(password);
+        }
+    } catch (err) {
+        throw new Error('Error al logear: ' + err.message);
+    }
+}
+
+
+
 modules.exports = {
     register,
+
 }
